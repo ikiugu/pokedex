fa565d33c4697395e605768bba2fca99043d2a2f
ikiugu <ikiugualf@gmail.com>
2025-10-15 00:38:55 +0300

    feat: added di

---
+++ app/src/main/java/com/ikiugu/oldmutual/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.ikiugu.oldmutual.di
+
+import com.ikiugu.oldmutual.data.repository.PokemonRepositoryImpl
+import com.ikiugu.oldmutual.domain.repository.PokemonRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindPokemonRepository(
+        pokemonRepositoryImpl: PokemonRepositoryImpl
+    ): PokemonRepository
+}
