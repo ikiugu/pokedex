b8f9e7a3bd876230c932a18b176312ea97310048
ikiugu <ikiugualf@gmail.com>
2025-10-15 06:34:41 +0300

    refactor: update app module and fix imports

--- app/src/main/java/com/ikiugu/pokedex/domain/entity/Ability.kt
+++
@@ -1,6 +0,0 @@
-package com.ikiugu.pokedex.domain.entity
-
-data class Ability(
-    val name: String,
-    val isHidden: Boolean
-)
--- app/src/main/java/com/ikiugu/pokedex/domain/entity/PokemonStat.kt
+++
@@ -1,7 +0,0 @@
-package com.ikiugu.pokedex.domain.entity
-
-data class PokemonStat(
-    val name: String,
-    val baseStat: Int,
-    val effort: Int
-)
